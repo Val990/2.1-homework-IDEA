5e359acfb8584c6971ba9b9cd664c34c87bc4e1e
Valeriya <tmcxezsim5nn@mail.ru>
2021-09-09 14:19:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int ticketAmount = 1_000;
+        int rublesFor1Mile = 20;
+        int bonusMiles = ticketAmount / rublesFor1Mile;
+        System.out.println(bonusMiles);
+    }
+}
+

38494f012ba561cc2f78c451b9090a0a7855d641
Valeriya <tmcxezsim5nn@mail.ru>
2021-09-10 15:14:54 +0300

    src/Main.java changed

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         int ticketAmount = 1_000;
         int rublesFor1Mile = 20;
         int bonusMiles = ticketAmount / rublesFor1Mile;
-        System.out.println(bonusMiles);
+        System.out.println("Бонусные мили:" + bonusMiles);
     }
 }
 
